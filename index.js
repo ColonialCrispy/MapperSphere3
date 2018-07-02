3f0309f23b526103a05d795e825a9f6600f53374
ColonialCrispy <crispindark@gmail.com>
2018-07-02 18:40:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -137,6 +137,37 @@ client.on('message', async (message) => {
 
     }
     
+    if (message.content.startsWith(`m!commie`)) {
+        let mUser = message.mentions.users.first()
+        let mUser2 = message.mentions.users.last()
+        const args29 = cont.slice(1)
+        if(!mUser) return message.channel.send(`Please specifify a user!`)
+        const mUserA = mUser.avatarURL
+        var imagetobase = `https://cdn.discordapp.com/attachments/358448632812535820/463432544831012864/image.png`;
+        Jimp.read(mUser.avatarURL, function (err, imagetouse) {
+            if (err) throw err;
+            imagetouse.quality(60)
+                      .resize(176, 176)
+                      .write("imagetouse.jpg");
+            Jimp.read(mUser2.avatarURL, function (err, imagetouse2) {
+            if (err) throw err;
+            imagetouse2.quality(60)
+                      .resize(191, 169)
+                      .write("imagetouse2.jpg");
+            Jimp.read(imagetobase, function (err, mydude) {
+                if (err) throw err;
+                mydude.quality(60)
+                      .composite( imagetouse, 473, 0 )
+                      .composite( imagetouse2, 84, 23)
+                      .write("commie.jpg");
+                mydude.getBuffer('image/jpeg', (err, buf) => {
+                    if (err) return err
+                    message.channel.send({files: [{attachment: buf, name: 'commie.jpg'}]})
+                }
+            )})}
+        )})
+    }
+    
     if (message.content.startsWith(`m!trans`)) {
         let mUser = message.mentions.users.first()
         const args29 = cont.slice(1)
